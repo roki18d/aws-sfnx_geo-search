c16142b2457fc65710155699da690ce33c8a2ab0
Hiroki Yamagishi <Hiroki11.Yamagishi@jp.nttdata.com>
2022-02-25 02:17:48 +0900

    update

--- services/write-dynamodb/lambda_function.py
+++ services/write-dynamodb/lambda_function.py
@@ -15,15 +15,20 @@ class UnexpectedError(Exception):
 def lambda_handler(event, context): 
 
     dynamodb_table_name = 'sfnx-DynamoDB-APIResultSets'
+    keys = ["placeinfo", ""]
+    api_result_sets = dict()
 
     try: 
         states_execution_id = event["states_execution_id"]
         query = event["query"]
         result_set = event["result_set"]
-
+        for result in result_set:
+            for key in keys: 
+                if key in result.keys(): 
+                    api_result_sets[key] = result[key]["body"]["results"]
         
 
-        response_body = {"report": None}
+        response_body = {"api_result_sets": api_result_sets}
         return {'statusCode': 200, 'body': response_body}
 
     except Exception as e:
